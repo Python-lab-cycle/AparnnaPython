c0691d881d1e3300a4e4e1c5eb577e2b7d6a2ce2
aparnnabimal <76677042+aparnnabimal@users.noreply.github.com>
2021-02-14 12:28:02 +0530

    Delete 15.COLORLIST.py

--- 15.COLORLIST.py
+++
@@ -1,6 +0,0 @@
-#  Print out all colors from color-list1 not contained in color-list2
-
-colorlist1 = set(["White", "Black", "Red"])
-colorlist2 = set(["Red", "Green"])
-
-print(colorlist1.difference(colorlist2))
