c40c992ba3288149113c00a934c892fcfd9a327f
aparnnabimal <76677042+aparnnabimal@users.noreply.github.com>
2021-02-14 12:26:31 +0530

    Delete 11.BIGGEST 3 NUMBERS.py

--- 11.BIGGEST 3 NUMBERS.py
+++
@@ -1,6 +0,0 @@
-a = float(input("Enter The First Number:\n"))
-b = float(input("Enter the Second Number:\n"))
-c = float(input("Enter the Third Number:\n"))
-
-c = max(a, b, c)
-print("Greatest Among Three Numbers: ", c)
