a93d2690958f0b7fe0da19cbd51db298aac20d26
aparnnabimal <76677042+aparnnabimal@users.noreply.github.com>
2021-02-14 12:29:02 +0530

    Delete 18.MERGE DIRECTORIES.py

--- 18.MERGE DIRECTORIES.py
+++
@@ -1,9 +0,0 @@
-#Merge Two Dictionary
-
-d1={'a':100,'b':200}
-d2={'c':300,'d':800}
-print("Dictionary 1=:",d1)
-print("Dictionary 2=:",d2)
-d=d1.copy()
-d.update(d2)
-print("The Merged Dictionary:",d)
