3d9bd44962ca10ec8b8f1a9cdbe2d7f8d73fd8cf
aparnnabimal <76677042+aparnnabimal@users.noreply.github.com>
2021-02-14 12:28:23 +0530

    Delete 16.STRING BY SWAPPING.py

--- 16.STRING BY SWAPPING.py
+++
@@ -1,9 +0,0 @@
-# Create a single string separated with space from two strings by swapping the character at position 1.
-
-
-a = input("Enter the value:\n")
-b = input("Enter the another value:\n")
-x = a[0:2]
-a = a.replace(a[0:2], b[0:2])
-b = b.replace(b[0:2], x)
-print(a, b)
