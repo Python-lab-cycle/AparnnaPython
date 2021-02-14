ad252e96647382ee3f39cd085c7f5b5aa52e88cb
aparnnabimal <76677042+aparnnabimal@users.noreply.github.com>
2021-02-14 12:30:44 +0530

    Delete 2.LEAP YEARS.py

--- 2.LEAP YEARS.py
+++
@@ -1,8 +0,0 @@
-# Display future leap years from current year to a final year entered by user.
-
-print("______Print Leap Year between two given years_____")
-startyear=int(input("Enter the Start Year:"))
-endyear=int(input("Enter the End Year:"))
-for year in range(startyear,endyear):
-    if ((year % 4 == 0) and (year % 100!=0) or (year % 400==0)):
-          print(year)
