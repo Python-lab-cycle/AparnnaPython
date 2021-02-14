4696de54478f2a256e878f7a650d931f1c4fd235
aparnnabimal <76677042+aparnnabimal@users.noreply.github.com>
2021-02-14 12:35:36 +0530

    Delete 9.CHARACTER CHANGED'.py

--- 9.CHARACTER CHANGED'.py
+++
@@ -1,4 +0,0 @@
-#Create a string from given string where first and last characters exchanged
-
-str1=input("Enter a String:\n")
-print("String after swapping first and last character:\n",(str1[-1:]),(str1[1:-1]),(str1[:1]))
