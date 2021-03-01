da445933c208e2836d281fcf8143e192257e30d9
aparnnabimal <76677042+aparnnabimal@users.noreply.github.com>
2021-03-01 16:00:16 +0530

    Add files via upload

---
+++ employee1Column.py
@@ -0,0 +1,8 @@
+import csv
+with open ('Employee1.csv',newline='')as csvfile1:
+    data=csv.DictReader(csvfile1)
+    print("empno empname")
+    print(".......")
+    for row in data:
+        print(row['Emp No'],row['Empname'])
+        
